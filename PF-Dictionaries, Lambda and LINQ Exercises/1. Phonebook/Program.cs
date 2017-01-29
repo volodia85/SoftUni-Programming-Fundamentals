accdc397eb27e54a253b6022ee8e7820c84d5f84
volodia <volodia@mail.bg>
2017-01-29 21:04:23 +0200

    exercise

---
+++ PF-Dictionaries, Lambda and LINQ Exercises/1. Phonebook/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+
+            Dictionary<string, string> entry = new Dictionary<string, string>();
+            List<string> search = new List<string>();
+
+            string type = input[0];
+            string name = input[1];
+            string number = input[2];
+            foreach (var item in input)
+            {
+                if (type == "A")
+                {
+                    entry[name] = number;
+                }
+                else if (type == "S")
+                {
+                    search.Add(name);
+                }
+            }
+            if (entry.Equals(search[0]))
+            {
+                Console.WriteLine("{0} -> {1}", name, entry[name]);
+            }
+            else
+            {
+                Console.WriteLine("Contact {0} does not exist.", name);
+            }
+
+        }
+    }
+}
