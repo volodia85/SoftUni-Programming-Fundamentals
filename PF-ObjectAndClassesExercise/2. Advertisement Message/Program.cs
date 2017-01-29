7508ec4817451c4690474bbfbf6e92fa8c866884
volodia <volodia@mail.bg>
2017-01-29 21:07:29 +0200

    exercise

---
+++ PF-ObjectAndClassesExercise/2. Advertisement Message/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Advertisement_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phrase = new string[] { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product." };
+            string[] events = new string[] { "Now I feel good.", "I have succeeded with this product.", "Makes miracles.I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+            string[] author = new string[] { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            string[] city = new string[] { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            Random rnd = new Random();
+            var phrase1 = 0;
+            var events1 = 0;
+            var author1 = 0;
+            var city1 = 0;
+
+            int numOfMessege = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfMessege; i++)
+            {
+                phrase1 = rnd.Next(0, phrase.Length);
+                events1 = rnd.Next(0, events.Length);
+                author1 = rnd.Next(0, author.Length);
+                city1 = rnd.Next(0, city.Length);
+                Console.WriteLine("{0} {1} {2} - {3}", string.Join(" ", phrase[phrase1]), string.Join(" ", events[events1]),
+               string.Join(" ", author[author1]), string.Join(" ", city[city1]));
+            }
+        }
+    }
+}
---
+++ PF-ObjectAndClassesExercise/5. Book Library/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Book_Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bookCount = int.Parse(Console.ReadLine());
+
+            Library Library = new Library();
+
+            List<Book> books = new List<Book>();
+
+            for (int i = 0; i < bookCount; i++)
+            {
+                books.Add(Readbooks());
+            }
+            Library.books = books;
+            
+
+            Dictionary<string, List<Book>> booksByAuthor =  CreateBooksByAuthor(Library );
+            foreach (var authorEntry in booksByAuthor.OrderByDescending(b => b.Value.Select(a => a.price).Sum()).ThenBy(a => a.Key))
+            
+            {
+                decimal priceTotal = authorEntry.Value.Select(x => x.price).Sum();
+                Console.WriteLine($"{authorEntry.Key} -> {priceTotal}");
+            }
+        }
+
+        private static Dictionary<string, List<Book>> CreateBooksByAuthor(Library Library)
+        {
+            Dictionary<string, List<Book>> booksByAuthor = new Dictionary<string, List<Book>>();
+            foreach (var book in Library.books)
+            {
+                if (!booksByAuthor.ContainsKey(book.author))
+                {
+                    booksByAuthor.Add(book.author, new List<Book>());
+                }
+                booksByAuthor[book.author].Add(book);
+            }
+            return booksByAuthor;
+        }
+
+        private static Book Readbooks()
+        {
+            Book book = new Book();
+
+            string[] input = Console.ReadLine().Split();
+            book.title = input[0];
+            book.author = input[1];
+            book.publisher = input[2];
+            book.releaseDate = DateTime.Parse(input[3]);
+            book.ISBN = int.Parse(input[4]);
+            book.price = decimal.Parse(input[5]);
+
+            return book;
+        }
+    }
+
+    class Book
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public string publisher { get; set; }
+        public DateTime releaseDate { get; set; }
+        public int ISBN { get; set; }
+        public decimal price { get; set; }
+    }
+
+    class Library
+    {
+        public string name { get; set; }
+        public List<Book> books { get; set; }
+    }
+}
