fbabfc0fbc0b6f42f0d568bf16a3c58e21896c1d
volodia <volodia@mail.bg>
2017-02-06 21:45:27 +0200

    new exercises

--- PF-ArraysAndLists/3.Array Manipulator/Program.cs
+++ PF-ArraysAndLists/3.Array Manipulator/Program.cs
@@ -11,26 +11,86 @@ namespace _3.Array_Manipulator
         static void Main(string[] args)
         {
             List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
-            var nextInput = "";
+            List<string> nextInput = new List<string>();
             int[] nn = new int[input.Count];
+            var endInput = input.Count;
             while (!input.Equals("print"))
             {
-                nextInput = Console.ReadLine();
+                nextInput = Console.ReadLine().Split().ToList();
                 if (nextInput.Contains("add"))
                 {
-                    var next = nextInput.Remove(nextInput[0]);
-                    next.Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
-                    input[nextInput[1]] = nextInput[1];
+                    nextInput.Remove(nextInput[0]);
+                    List<int> nums = nextInput.Select(int.Parse).ToList();
+                    int index = nums[0];
+                    int num = nums[1];
+                    input.Insert(index, num);
 
                 }
                 else if (nextInput.Contains("addMany"))
                 {
-                    nextInput.Split().Select(int.Parse).ToList();
-                    input.Add(nextInput[1]);
-                }else if (nextInput.Contains("contains"))
-                {
+                    nextInput.Remove(nextInput[0]);
+                    List<int> nums = nextInput.Select(int.Parse).ToList();
+                    int index = nums[0];
+                    int num = nums[1];
+                    input.AddRange(nums);
                     
                 }
+                else if (nextInput.Contains("contains"))
+                {
+                    nextInput.Remove(nextInput[0]);
+                    List<int> nums = nextInput.Select(int.Parse).ToList();
+                    int num = nums[0];
+                    if (input.Contains(num))
+                    {
+                        Console.WriteLine(nums.IndexOf(num));
+                    }
+                    else
+                    {
+                        Console.WriteLine("-1");
+                    }
+                }
+                else if (nextInput.Contains("remove"))
+                {
+                   nextInput.Remove(nextInput[0]);
+                    List<int> nums = nextInput.Select(int.Parse).ToList();
+                    int index = nums[0];
+                    input.RemoveAt(index);
+                    
+                }
+                else if (nextInput.Contains("shift"))
+                {
+                    nextInput.Remove(nextInput[0]);
+                    List<int> nums = nextInput.Select(int.Parse).ToList();
+                    int rotations = nums[0];
+                    RotateRight(input,rotations);
+                }else if (nextInput.Contains("sumPairs"))
+                {
+
+                    for (int i = 1; i < input.Count; i++)
+                    {
+                        var sum = input[i-1] + input[i];
+
+                    }
+                }
+            Console.WriteLine("[{0}]",string.Join(" ",input));
+            }
+        }
+        public static void RotateRight(List<int> input, int right)
+        {
+            for (var i = 0; i < right; i += 1)
+            {
+                RotateRightOne(input);
+            }
+        }
+
+        public static void RotateRightOne(List<int> input)
+        {
+            var last = input.Count - 1;
+            for (var i = 0; i < last; i += 1)
+            {
+                input[i] ^= input[last];
+                input[last] ^= input[i];
+                input[i] ^= input[last];
             }
         }
     }
---
+++ PF-ArraysAndLists/Substring/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+public class Substring_broken
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        int jump = int.Parse(Console.ReadLine());
+
+        const char Search = 'p';
+        bool hasMatch = false;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == Search)
+            {
+                hasMatch = true;
+                string matchedString;
+                int endIndex = jump + 1;
+
+                if (endIndex + i <= text.Length)
+                {
+                    matchedString = text.Substring(i, endIndex);
+                }
+                else
+                {
+                    matchedString = text.Substring(i);
+                } 
+                Console.WriteLine(matchedString);
+                i += jump;
+            }
+        }
+
+        if (!hasMatch)
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
