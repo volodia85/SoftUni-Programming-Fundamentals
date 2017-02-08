c91100f35f0ce31154e16462c6fa5f664ea6c88f
volodia <volodia@mail.bg>
2017-02-08 23:54:06 +0200

    new exercises

--- PF-Dictionaries, Lambda and LINQ Exercises/06. User Logs/Program.cs
+++ PF-Dictionaries, Lambda and LINQ Exercises/06. User Logs/Program.cs
@@ -4,12 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _06.User_Logs
+namespace LINQ
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine().Trim();
+
+            var userLogs = new SortedDictionary<string, SortedDictionary<string, int>>();
+            while (!input.Equals("end"))
+            {
+                string[] log = input.Split(new char[] { '=', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string username = log[5];
+                string IP = log[1];
+                int counter = 1;
+
+                if (!userLogs.ContainsKey(username))
+                {
+                    userLogs.Add(username, new SortedDictionary<string, int>());
+                }
+                if (!userLogs[username].ContainsKey(IP))
+                {
+                    userLogs[username].Add(IP, counter);
+                }
+                else userLogs[username][IP]++;
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var user in userLogs)
+            {
+                Console.WriteLine($"{user.Key}: ");
+                foreach (var log in user.Value)
+                {
+                    var thing = log.Key;
+                    if (log.Key != user.Value.Keys.Last())
+                    {
+                        Console.Write($"{log.Key} => {log.Value}, ");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{log.Key} => {log.Value}.");
+                    }
+                }
+            }
         }
     }
 }
---
+++ PF-Dictionaries, Lambda and LINQ Exercises/7.Population Counter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Population_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var countryPopulation = new Dictionary<string, Dictionary<string, int>>();
+
+            while (!input.Contains("report"))
+            {
+                var line = input.Split('|');
+                var city = line[0].ToString();
+                var country = line[1].ToString();
+                var population = int.Parse(line[2]);
+
+                countryPopulation[country] = new Dictionary<string, int>();
+                countryPopulation[country].Add(city, population);
+                if (countryPopulation.ContainsKey(city))
+                {
+                    countryPopulation[country].Keys.Distinct();
+                    countryPopulation[country].Values.Sum();
+                }
+                
+            }
+            foreach (var item in countryPopulation)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
