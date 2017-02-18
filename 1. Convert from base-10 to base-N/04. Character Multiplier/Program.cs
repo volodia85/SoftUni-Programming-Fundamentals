2d0ae75f051c157ec0f1f2ca2fd4ce95e05b2796
volodia <volodia@mail.bg>
2017-02-18 20:18:05 +0200

    exercise new exercise

--- 1. Convert from base-10 to base-N/04. Character Multiplier/Program.cs
+++ 1. Convert from base-10 to base-N/04. Character Multiplier/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _04.Character_Multiplier
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string[] input = Console.ReadLine().Split();
+
+        string str1 = input[0];
+        string str2 = input[1];
+
+        int minLenght = Math.Min(str1.Length, str2.Length);
+        int maxLenght = Math.Max(str1.Length, str2.Length);
+        int sum = 0;
+
+        for (int i = 0; i < minLenght; i++)
         {
+            sum += MultiplyChars(str1[i], str2[i]);
         }
+
+        if (str1.Length != str2.Length)
+        {
+            string longerInput = str1.Length > str2.Length ? longerInput = str1 : longerInput = str2;
+            for (int i = minLenght; i < maxLenght; i++)
+            {
+                sum += longerInput[i];
+            }
+        }
+        Console.WriteLine(sum);
+    }
+
+    static int MultiplyChars(char let1, char let2)
+    {
+        int multiply = let1 * let2;
+        return multiply;
     }
 }
